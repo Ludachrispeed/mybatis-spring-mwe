ea1971b76cc6072ac0d32d299dd015df73843441
Chris Clark <ludachrispeed@gmail.com>
2014-01-11 12:01:14 -0500

    removed AbstractServiceTest to make test more readable

--- src/test/java/example/services/PersonServiceTest.java
+++ src/test/java/example/services/PersonServiceTest.java
@@ -4,19 +4,27 @@ package example.services;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import example.appconfig.DataSourceConfig;
+import example.appconfig.MyBatisConfig;
 import example.domain.Person;
 import example.services.PersonService;
 
 @ContextConfiguration
-public class PersonServiceTest extends AbstractServiceTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+public class PersonServiceTest {
 
     @Configuration
     @ComponentScan(basePackageClasses = PersonService.class)
+    @Import({DataSourceConfig.class, MyBatisConfig.class})
     static class LocalConfig {
         // nothing to declare - we get all the beans we need through component scanning
     }
